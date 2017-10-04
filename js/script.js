b51267cbf548ac0f7593a5c8bbd54b735adecd6f
Dan <daniel.munozdev@gmail.com>
2017-10-03 21:12:24 -0700

    Add li to ul with prepend when button is clicked

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 $('a#hello').click(function() {
-  console.log($(this));
+  $('ul#user').prepend('<li>Hello!</li>');
+  $('ul#webpage').prepend('<li>Why hello there!</li>');
 });
 
 $('a#goodbye').click(function() {
-  console.log($(this));
+  $('ul#user').prepend('<li>Goodbye!</li>');
+  $('ul#webpage').prepend('<li>Goodbye, dear user!</li>');
 });
 
 $('a#stop').click(function() {
-  console.log($(this));
+  $('ul#user').prepend('<li>Stop copying me!</li>');
+  $('ul#webpage').prepend('<li>Pardon me. I meant no offense.</li>');
 });
