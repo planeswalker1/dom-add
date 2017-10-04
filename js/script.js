95a5a771926871537966631ee236683b8d388f99
Dan <daniel.munozdev@gmail.com>
2017-10-03 21:19:56 -0700

    Add prepend function to reuse code

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
-$('a#hello').click(function() {
-  $('ul#user').prepend('<li>Hello!</li>');
-  $('ul#webpage').prepend('<li>Why hello there!</li>');
+function prepend(selector, message) {
+  return $(selector).prepend('<li>' + message + '</li>');
+}
 
+$('a#hello').click(function() {
+  prepend('ul#user', 'Hello!');
+  prepend('ul#webpage', 'Why hello there!');
+  // old code
+  // $('ul#user').prepend('<li>Hello!</li>');
+  // $('ul#webpage').prepend('<li>Why hello there!</li>');
   $('ul#user').children('li').first().click(function() {
     $(this).remove();
   });
@@ -11,9 +17,11 @@ $('a#hello').click(function() {
 });
 
 $('a#goodbye').click(function() {
-  $('ul#user').prepend('<li>Goodbye!</li>');
-  $('ul#webpage').prepend('<li>Goodbye, dear user!</li>');
-
+  prepend('ul#user', 'Goodbye!');
+  prepend('ul#webpage', 'Goodbye, dear user!');
+  // old code
+  // $('ul#user').prepend('<li>Goodbye!</li>');
+  // $('ul#webpage').prepend('<li>Goodbye, dear user!</li>');
   $('ul#user').children('li').first().click(function() {
     $(this).remove();
   });
@@ -23,9 +31,11 @@ $('a#goodbye').click(function() {
 });
 
 $('a#stop').click(function() {
-  $('ul#user').prepend('<li>Stop copying me!</li>');
-  $('ul#webpage').prepend('<li>Pardon me. I meant no offense.</li>');
-
+  prepend('ul#user', 'Stop copying me!');
+  prepend('ul#webpage', 'pardon me. I meant no offense.');
+  // old code
+  // $('ul#user').prepend('<li>Stop copying me!</li>');
+  // $('ul#webpage').prepend('<li>Pardon me. I meant no offense.</li>');
   $('ul#user').children('li').first().click(function() {
     $(this).remove();
   });
