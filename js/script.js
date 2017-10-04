12b53269cab77d044299e30ad9c90f2f0e976000
Dan <daniel.munozdev@gmail.com>
2017-10-03 21:14:05 -0700

    Remove li when li is clicked from respective ul

--- js/script.js
+++ js/script.js
@@ -1,14 +1,35 @@
 $('a#hello').click(function() {
   $('ul#user').prepend('<li>Hello!</li>');
   $('ul#webpage').prepend('<li>Why hello there!</li>');
+
+  $('ul#user').children('li').first().click(function() {
+    $(this).remove();
+  });
+  $('ul#webpage').children('li').first().click(function() {
+    $(this).remove();
+  });
 });
 
 $('a#goodbye').click(function() {
   $('ul#user').prepend('<li>Goodbye!</li>');
   $('ul#webpage').prepend('<li>Goodbye, dear user!</li>');
+
+  $('ul#user').children('li').first().click(function() {
+    $(this).remove();
+  });
+  $('ul#webpage').children('li').first().click(function() {
+    $(this).remove();
+  });
 });
 
 $('a#stop').click(function() {
   $('ul#user').prepend('<li>Stop copying me!</li>');
   $('ul#webpage').prepend('<li>Pardon me. I meant no offense.</li>');
+
+  $('ul#user').children('li').first().click(function() {
+    $(this).remove();
+  });
+  $('ul#webpage').children('li').first().click(function() {
+    $(this).remove();
+  });
 });
