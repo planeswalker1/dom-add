f0fd391fa7526f4387028f9b8d94803c200c758c
Dan <daniel.munozdev@gmail.com>
2017-10-03 21:09:15 -0700

    Add event listener to buttons

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$('a#hello').click(function() {
+  console.log($(this));
+});
+
+$('a#goodbye').click(function() {
+  console.log($(this));
+});
+
+$('a#stop').click(function() {
+  console.log($(this));
+});
